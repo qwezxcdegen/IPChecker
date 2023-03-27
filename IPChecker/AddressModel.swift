28fe85d74ffe1ed62be16a42cf65610e99f114c0
Stepan <ifomstep@icloud.com>
2023-03-27 11:41:57 +0500

    Add ip address model

---
+++ IPChecker/AddressModel.swift
@@ -0,0 +1,14 @@
+//
+//  AddressModel.swift
+//  IPChecker
+//
+//  Created by Степан Фоминцев on 27.03.2023.
+//
+
+import Foundation
+
+// MARK: - Address
+struct Address: Codable {
+    let country, countryCode, regionName: String
+    let city, zip, timezone, org, query: String
+}
