0ce6b4c6c99e07b145439be64be4062f44f38534
qwezxcdegen <ifomstep@icloud.com>
2023-03-29 22:37:55 +0500

    Add mapkit logic

--- IPChecker/ViewController.swift
+++ IPChecker/ViewController.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import MapKit
 
 final class ViewController: UIViewController {
-
     @IBOutlet weak private var addressTextField: UITextField!
-    
     @IBOutlet weak private var resultTextView: UITextView!
-    
     @IBOutlet weak private var selfAddressLabel: UILabel!
+    @IBOutlet weak private var mapView: MKMapView!
     
     private var selfAddress = ""
     
@@ -54,7 +53,7 @@ final class ViewController: UIViewController {
     }
     
     private func fetchAddressData(ipAddress: String) {
-        guard let url = URL(string: "http://ip-api.com/json/\(ipAddress)?fields=country,countryCode,regionName,city,zip,timezone,org,query") else {  self.resultTextView.text = "Error"; return }
+        guard let url = URL(string: "http://ip-api.com/json/\(ipAddress)?fields=country,countryCode,regionName,city,zip,timezone,org,query,lat,lon") else {  self.resultTextView.text = "Error"; return }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
@@ -62,13 +61,18 @@ final class ViewController: UIViewController {
             guard let data, error == nil else { self.resultTextView.text = "Error"; return }
             guard let addressData = try? JSONDecoder().decode(Address.self, from: data) else { self.resultTextView.text = "Error"; return }
             DispatchQueue.main.async {
+                self.mapView.removeAnnotations(self.mapView.annotations)
                 self.resultTextView.text = "Country: \(addressData.country)\nRegion: \(addressData.regionName)\nCity: \(addressData.city)\nPostal code: \(addressData.zip)\nTimezone: \(addressData.timezone)\nOrganization: \(addressData.org)"
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2D(latitude: addressData.lat, longitude: addressData.lon)
+                self.mapView.setCamera(MKMapCamera(lookingAtCenter: CLLocationCoordinate2D(latitude: addressData.lat, longitude: addressData.lon), fromDistance: 100000, pitch: 0, heading: 0), animated: true)
+                self.mapView.addAnnotation(annotation)
             }
         }.resume()
     }
     
     private func fetchSelfAddress() {
-        guard let url = URL(string: "http://ip-api.com/json/?fields=country,countryCode,regionName,city,zip,timezone,org,query") else { self.selfAddressLabel.text = "Error"; return }
+        guard let url = URL(string: "http://ip-api.com/json/?fields=country,countryCode,regionName,city,zip,timezone,org,query,lat,lon") else { self.selfAddressLabel.text = "Error"; return }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         URLSession.shared.dataTask(with: request) { data, response, error in
